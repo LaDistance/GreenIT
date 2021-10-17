504994b092fbd1071d87624d0b97b25348f2e88a
Antoine LAURENT <antoine.laurent@reseau.eseo.fr>
2021-10-17 23:09:49 +0200

    Fixed import

--- js/scripts2.js
+++ js/scripts2.js
@@ -12,20 +12,17 @@ function openDialog() {
 function submitForm(event) {
     var reader = new FileReader();
     reader.onload = function(event){
-        console.log("Text : ", reader.result);
         loadAnswers(event.target.result);
         
     }
     var file = document.getElementById('fileid').files[0];
-    console.log("File : ", file);
     reader.readAsText(file);
     
 }
 function loadAnswers(jsonString){
     answers = JSON.parse(jsonString);
     let spans = document.getElementsByTagName("span");
-    console.log("spans : ", spans);
-    logAnswers();
+
     for(let i=0; i<spans.length; i++){
 
         let name = spans[i].getAttribute("name");
@@ -37,7 +34,6 @@ function loadAnswers(jsonString){
             }
             else{
                 spans[i].innerHTML = answers[name][id].toLocaleString();
-                console.log("Changed span with id : " + id + " to : ", answers[name][id]);
             }
         }
         else if(name=="avion"){
